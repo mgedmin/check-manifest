ec64db71f7c62f0d6b74014807f7c9930903e517
Marius Gedminas <marius@gedmin.as>
2014-02-03 11:45:00 +0200

    Windows doesn't like Unicode arguments to subprocess.Popen()
    I'm actually surprised -- don't all the Win32 APIs have Unicode
    versions?  Eh, Python.
    
    Related to issue #23.

--- tests.py
+++ tests.py
@@ -706,6 +706,9 @@ class TestZestIntegration(unittest.TestCase):
 class VCSHelper(object):
 
     def _run(self, *command):
+        # Windows doesn't like Unicode arguments to subprocess.Popen():
+        # https://github.com/mgedmin/check-manifest/issues/23#issuecomment-33933031
+        command = [s.encode(locale.getpreferredencoding()) for s in command]
         p = subprocess.Popen(command, stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT)
         stdout, stderr = p.communicate()
