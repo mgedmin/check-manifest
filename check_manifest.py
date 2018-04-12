f5daf35ffa5ddc05eb87bb9217c8afed73b28b20
Marius Gedminas <marius@gedmin.as>
2018-04-12 16:13:44 +0300

    Back to development: 0.38

--- check_manifest.py
+++ check_manifest.py
@@ -42,7 +42,7 @@ except ImportError:
     import configparser as ConfigParser
 
 
-__version__ = '0.37'
+__version__ = '0.38.dev0'
 __author__ = 'Marius Gedminas <marius@gedmin.as>'
 __licence__ = 'MIT'
 __url__ = 'https://github.com/mgedmin/check-manifest'
