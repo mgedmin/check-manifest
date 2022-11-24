45c925f0b61e471c27f79eecf10ae207b8f91de5
Marius Gedminas <marius@gedmin.as>
2022-11-24 12:59:35 +0200

    isort insists on this blank line, wtf?

--- tests.py
+++ tests.py
@@ -96,6 +96,7 @@ class Tests(unittest.TestCase):
 
     def test_run_failure(self):
         from check_manifest import CommandFailed, run
+
         # /bin/false can return any non-zero status code, e.g. it returns 255
         # on OpenIndiana: https://github.com/mgedmin/check-manifest/issues/162
         command = [sys.executable, '-c', 'import sys; sys.exit(1)']
